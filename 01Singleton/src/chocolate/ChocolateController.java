706a25adffd68a5428998f9bede49eaceb60881c
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-10-07 00:59:06 +0200

    czekoladowe zadanie

--- 01Singleton/src/chocolate/ChocolateController.java
+++ 01Singleton/src/chocolate/ChocolateController.java
@@ -10,5 +10,12 @@ public class ChocolateController {
 		// will return the existing instance
 		ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
 		System.out.println(boiler == boiler2);
+		
+		// Zadanie:
+		//   Czy istnieje mo¿liwoœæ by dwa w¹tki wykonuj¹ce  
+		//        ChocolateBoiler.getInstance()
+		//   otrzyma³y dwie ró¿ne instancje klasy ChocolateBoiler ?
+		//   Je¿eli tak zaproponuj przeplot instrukcji metody getInstance, która 
+		//   do tego doprowadzi.
 	}
 }
