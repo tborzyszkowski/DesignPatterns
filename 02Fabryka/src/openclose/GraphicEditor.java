262da2d5895cd33c710a65edb66097b10b602a67
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-10-08 23:02:24 +0200

    Open close Principle

---
+++ 02Fabryka/src/openclose/GraphicEditor.java
@@ -0,0 +1,24 @@
+package openclose;
+
+//Open-Close Principle - Good example
+class GraphicEditor {
+	public void drawShape(Shape s) {
+		s.draw();
+	}
+}
+
+abstract class Shape {
+	abstract void draw();
+}
+
+class Rectangle extends Shape  {
+	public void draw() {
+		System.out.println("Rectangle");
+	}
+} 
+
+class Circle extends Shape  {
+	public void draw() {
+		System.out.println("Circle");
+	}
+} 
