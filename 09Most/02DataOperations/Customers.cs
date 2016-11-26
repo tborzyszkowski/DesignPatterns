0df9f34989691ecda78f899bf987b5a9f8151a02
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-11-26 23:00:02 +0100

    Bridge Customers Data

---
+++ 09Most/02DataOperations/Customers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02DataOperations {
+
+    // The 'RefinedAbstraction' class
+
+    class Customers : CustomersBase {
+        // Constructor
+        public Customers(string group)
+          : base(group) {
+        }
+
+        public override void ShowAll() {
+            // Add separator lines
+            Console.WriteLine();
+            Console.WriteLine("------------------------");
+            base.ShowAll();
+            Console.WriteLine("------------------------");
+        }
+    }
+}
