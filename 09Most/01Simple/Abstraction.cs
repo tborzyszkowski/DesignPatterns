ab5b6e5f6b033f2c76d5d724689abe8292568205
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-11-26 22:17:23 +0100

    Most Simple

---
+++ 09Most/01Simple/Abstraction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Simple {
+    class Abstraction {
+        protected Implementor implementor;
+
+        // Property
+        public Implementor Implementor
+        {
+            set { implementor = value; }
+        }
+
+        public virtual void Operation() {
+            implementor.Operation();
+        }
+    }
+}
