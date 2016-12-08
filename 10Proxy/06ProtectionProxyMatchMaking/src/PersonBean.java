2c9a35b3920513f346baf99e6442eb664eb48c92
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-12-08 23:39:26 +0100

    Protection proxy as Dynamic Proxy

---
+++ 10Proxy/06ProtectionProxyMatchMaking/src/PersonBean.java
@@ -0,0 +1,13 @@
+public interface PersonBean {
+ 
+	String getName();
+	String getGender();
+	String getInterests();
+	int getHotOrNotRating();
+ 
+    void setName(String name);
+    void setGender(String gender);
+    void setInterests(String interests);
+    void setHotOrNotRating(int rating); 
+ 
+}
