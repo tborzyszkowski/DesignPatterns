84ab97a9cf60132f6e116d23ee37da520ceddd4e
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-12-08 22:27:06 +0100

    GumBall & RMI

---
+++ 10Proxy/04GumballMonitorRMI/src/GumballMachineRemote.java
@@ -0,0 +1,7 @@
+import java.rmi.*;
+ 
+public interface GumballMachineRemote extends Remote {
+	public int getCount() throws RemoteException;
+	public String getLocation() throws RemoteException;
+	public State getState() throws RemoteException;
+}
---
+++ 10Proxy/04GumballMonitorRMI/src/GumballMonitorTestDrive.java
@@ -0,0 +1,27 @@
+import java.rmi.*;
+ 
+public class GumballMonitorTestDrive {
+ 
+	public static void main(String[] args) {
+		String[] location = {"rmi://santafe.mightygumball.com/gumballmachine",
+		                     "rmi://boulder.mightygumball.com/gumballmachine",
+		                     "rmi://seattle.mightygumball.com/gumballmachine"}; 
+ 
+		GumballMonitor[] monitor = new GumballMonitor[location.length];
+ 
+		for (int i=0;i < location.length; i++) {
+			try {
+           		GumballMachineRemote machine = 
+						(GumballMachineRemote) Naming.lookup(location[i]);
+           		monitor[i] = new GumballMonitor(machine);
+				System.out.println(monitor[i]);
+        	} catch (Exception e) {
+            	e.printStackTrace();
+        	}
+		}
+ 
+		for(int i=0; i < monitor.length; i++) {
+			monitor[i].report();
+		}
+	}
+}
