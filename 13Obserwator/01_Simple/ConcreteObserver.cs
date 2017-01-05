8f2de127ffcd1bb2dfebfa61d4e0e7c6726463b9
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-01-05 11:55:54 +0100

    Obserwator

---
+++ 13Obserwator/01_Simple/ConcreteObserver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Simple {
+    class ConcreteObserver : Observer {
+        private string _name;
+        private string _observerState;
+        private ConcreteSubject _subject;
+
+        // Constructor
+        public ConcreteObserver(
+          ConcreteSubject subject, string name) {
+            this._subject = subject;
+            this._name = name;
+        }
+
+        public override void Update() {
+            _observerState = _subject.SubjectState;
+            Console.WriteLine("Observer {0}'s new state is {1}",
+              _name, _observerState);
+        }
+
+        // Gets or sets subject
+        public ConcreteSubject Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+    }
+}
---
+++ 13Obserwator/02_Inwestors/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Inwestors {
+    class MainApp {
+        static void Main(string[] args) {
+            // Create IBM stock and attach investors
+            IBM ibm = new IBM("IBM", 120.00);
+            ibm.Attach(new Investor("Sorros"));
+            ibm.Attach(new Investor("Kowalski"));
+
+            // Fluctuating prices will notify investors
+            ibm.Price = 120.10;
+            ibm.Price = 121.00;
+            ibm.Price = 120.50;
+            ibm.Price = 120.75;
+
+        }
+    }
+}
