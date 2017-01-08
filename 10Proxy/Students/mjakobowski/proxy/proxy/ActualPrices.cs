88eb1abf14d5bf28c2e3225d5a0fe30334954951
micool91 <micool91@gmail.com>
2017-01-08 13:43:28 +0100

    simple remote proxy

---
+++ 10Proxy/Students/mjakobowski/proxy/proxy/ActualPrices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proxy
+{
+    class ActualPrices : IActualPrices
+    {
+        public string GoldPrice
+        {
+            get
+            {
+                // This value should come from a DB typically
+                return "4 756,92";
+            }
+        }
+
+        public string SilverPrice
+        {
+            get
+            {
+                // This value should come from a DB typically
+                return "190,52";
+            }
+        }
+
+        public string DollarToZloty
+        {
+            get
+            {
+                // This value should come from a DB typically
+                return "4.12";
+            }
+        }
+    }
+}
