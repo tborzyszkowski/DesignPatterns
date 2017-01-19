b5856fc8c66833e54ff15d1f33d59994c8e1f989
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-01-19 23:57:35 +0100

    Bankomat

---
+++ 18LancuchZobowiazan/03Bankomat/src/Dollar10Dispenser.java
@@ -0,0 +1,21 @@
+public class Dollar10Dispenser implements DispenseChain {
+
+	private DispenseChain chain;
+	
+	@Override
+	public void setNextChain(DispenseChain nextChain) {
+		this.chain=nextChain;
+	}
+
+	@Override
+	public void dispense(Currency cur) {
+		if(cur.getAmount() >= 10){
+			int num = cur.getAmount()/10;
+			int remainder = cur.getAmount() % 10;
+			System.out.println("Dispensing "+num+" 10$ note");
+			if(remainder !=0) this.chain.dispense(new Currency(remainder));
+		}else{
+			this.chain.dispense(cur);
+		}
+	}
+}
