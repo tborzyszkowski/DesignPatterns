04b283b34393d417f8cfbc1710c5f51a1082ff32
matismatis93 <mateuszmichalski93@gmail.com>
2017-01-22 11:15:22 +0100

    projekt_mmichalski
    projekt_mmichalski

---
+++ ProjektyStudentow/2017_Mateusz_Michalski/blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^weather/$', views.get_weather, name='weather'),
+    url(r'^copy/(?P<pk>[0-9]+)/$', views.post_copy, name='post_copy'),
+]
