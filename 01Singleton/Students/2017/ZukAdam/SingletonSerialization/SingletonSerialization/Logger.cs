8c578fac2a42244e7801dfc88914f7e7eff02ad7
Adam Żuk <adam.zuk@outlook.com>
2017-10-18 19:47:14 +0200

    Added SingletonSerialization project

---
+++ 01Singleton/Students/2017/ZukAdam/SingletonSerialization/SingletonSerialization/Logger.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using SingletonSerialization.Interfaces;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace SingletonSerialization
+{
+    public class Logger : ILogger
+    {
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+
+        protected static ILogger LoggerInstance;
+
+        protected Logger()
+        {
+            this.ForeColor = ConsoleColor.Gray;
+            this.BackColor = ConsoleColor.Black;
+        }
+
+        public static ILogger GetLoggerInstance(string fileName = "")
+        {
+            lock (LoggerInstance)
+            {
+                if (LoggerInstance == null && fileName == "")
+                {
+                    LoggerInstance = new Logger();
+                }
+                else if (fileName != "")
+                {
+                    LoggerInstance = Deserialize(fileName);
+                }
+                return LoggerInstance;
+            }
+        }
+
+        public virtual void Log(string text)
+        {
+            Console.BackgroundColor = this.BackColor;
+            Console.ForegroundColor = this.ForeColor;
+            Console.WriteLine(text);
+        }
+
+        public bool Serialize(string fileName)
+        {
+            using(var writer = new StreamWriter(Environment.CurrentDirectory + @"\" + fileName))
+            {
+                try
+                {
+                    var serializer = new XmlSerializer(typeof(Logger));
+                    serializer.Serialize(writer, GetLoggerInstance());
+
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    this.Log(e.Message);
+                    return false;
+                }
+            }
+        }
+
+        private static ILogger Deserialize(string fileName)
+        {
+            ILogger result = null;
+
+            using (var reader = new StreamReader(Environment.CurrentDirectory + @"\" + fileName))
+            {
+                var serializer = new XmlSerializer(typeof(Logger));
+                result = serializer.Deserialize(reader) as Logger;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 01Singleton/Students/2017/ZukAdam/SingletonSerialization/SingletonSerialization/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating new instance of logger, setting up new background color and serializing for test purposes
+            var logger = Logger.GetLoggerInstance() as Logger;
+            logger.BackColor = ConsoleColor.Red;
+
+            logger.Serialize("Logger.xml");
+
+
+        }
+    }
+}
