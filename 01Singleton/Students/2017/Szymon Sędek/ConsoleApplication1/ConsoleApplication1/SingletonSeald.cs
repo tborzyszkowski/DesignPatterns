052fe712ce8d8115fdf2eee1e3007b1316b978f2
Szymon Sędek <szymon.sedek@gmail.com>
2017-10-21 16:52:37 +0200

    NEW Szymon Sędek Singleton

---
+++ 01Singleton/Students/2017/Szymon Sędek/ConsoleApplication1/ConsoleApplication1/SingletonSeald.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public sealed class SingletonSeald
+    {
+        private static SingletonSeald _instance;
+
+        public static SingletonSeald Instance => _instance ?? (_instance = new SingletonSeald());
+
+        private SingletonSeald()
+        {
+
+        }
+    }
+}
---
+++ 01Singleton/Students/2017/Szymon Sędek/ConsoleApplication1/ConsoleApplication1/Testing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Testing : Singleton<Testing>
+    {
+
+        public void STH()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
