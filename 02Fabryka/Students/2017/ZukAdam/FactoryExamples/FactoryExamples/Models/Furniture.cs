600238da1eb5cc98cd7221e72acb4c2abde75cfd
Adam Żuk <adam.zuk@outlook.com>
2017-11-02 20:17:02 +0100

    Initial commit

---
+++ 02Fabryka/Students/2017/ZukAdam/FactoryExamples/FactoryExamples/Models/Furniture.cs
@@ -0,0 +1,26 @@
+﻿using FactoryExamples.Enums;
+using FactoryExamples.Interfaces;
+using System.Collections.Generic;
+
+namespace FactoryExamples.Models
+{
+    public abstract class Furniture
+    {
+        public string Name { get; set; }
+
+        public CategoryEnum Category { get; set; }
+
+        public List<IPart> Parts { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int Depth { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
