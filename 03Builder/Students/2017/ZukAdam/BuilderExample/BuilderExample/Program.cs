1ec097e0bb91375128720d282dbe9878d3cdd3d6
Adam Żuk <adam.zuk@outlook.com>
2017-11-16 07:58:45 +0100

    Initial commit

---
+++ 03Builder/Students/2017/ZukAdam/BuilderExample/BuilderExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BuilderExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit marine = new MarineBuilder().SetName("Marine_1")
+                                             .SetArmorPoints(5)
+                                             .SetAttackPoints(7)
+                                             .SetHealthPoints(10)
+                                             .SetSize(1);
+
+            marine.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 03Builder/Students/2017/ZukAdam/BuilderExample/BuilderExample/UnitBuilder.cs
@@ -0,0 +1,23 @@
+﻿namespace BuilderExample
+{
+    public abstract class UnitBuilder
+    {
+        protected Unit unit;
+
+        Unit Unit
+        {
+            get { return unit; }
+        }
+
+        public abstract UnitBuilder SetName(string name);
+        public abstract UnitBuilder SetArmorPoints(int armorPoints);
+        public abstract UnitBuilder SetHealthPoints(int healthPoints);
+        public abstract UnitBuilder SetAttackPoints(int attackPoints);
+        public abstract UnitBuilder SetSize(int size);
+
+        public static implicit operator Unit(UnitBuilder builder)
+        {
+            return builder.Unit;
+        }
+    }
+}
