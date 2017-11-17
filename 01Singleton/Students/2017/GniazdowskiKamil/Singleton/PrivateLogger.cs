8f601b61259521498e5491649e7814e1fa98885a
Kamil Gniazdowski <kgniazdowski@inf.ug.edu.pl>
2017-11-17 17:18:00 +0100

    Gniazdowski Kamil Singleton

---
+++ 01Singleton/Students/2017/GniazdowskiKamil/Singleton/PrivateLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton
+{
+    class PrivateLogger : LoggerBase<PrivateLogger>
+    {
+        protected override string Path
+        {
+            get
+            {
+                return AppDomain.CurrentDomain.BaseDirectory + "/Logs/Private/";
+            }
+            set { }
+        }
+    }
+}
---
+++ 01Singleton/Students/2017/GniazdowskiKamil/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainLogger.Instance.Log("Pierwszy log");
+            PrivateLogger.Instance.Log("Log prywatny");
+
+            Console.WriteLine(MainLogger.LastLog);
+            Console.WriteLine(PrivateLogger.LastLog);
+
+            Console.ReadKey();
+        }
+    }
+}
