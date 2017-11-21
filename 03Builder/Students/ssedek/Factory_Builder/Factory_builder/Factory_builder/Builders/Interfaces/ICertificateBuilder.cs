4497a17a2d5fcc727523caa1fca4db9ff3c99b4a
Szymon Sędek <szymon.sedek@gmail.com>
2017-11-21 16:42:26 +0100

    factory and builder

---
+++ 03Builder/Students/ssedek/Factory_Builder/Factory_builder/Factory_builder/Builders/Interfaces/ICertificateBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Factory_builder.Models;
+
+namespace Factory_builder.Builders.Interfaces
+{
+    public interface ICertificateBuilder : IBuilderAbstract<Certificate>
+    {
+        CertificateBuilder Initialize();
+        CertificateBuilder SetName(string name = "Certificate");
+        CertificateBuilder SetSize(int size = 12);
+
+    }
+}
---
+++ 03Builder/Students/ssedek/Factory_Builder/Factory_builder/Factory_builder/Models/Contract.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_builder.Models
+{
+    public class Contract : Document
+    {
+    }
+}
