eefc008f24880aaf6de334a454ebe1af35e2999a
Adam Żuk <adam.zuk@outlook.com>
2017-12-11 21:37:06 +0100

    Adapter example

---
+++ 06Adapter/Students/2017/ZukAdam/AdapterExample/AdapterExample/LegacyGfxLibrary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AdapterExample
+{
+    public class LegacyGfxLibrary
+    {
+        public string ObtainGraphicsChipsetInfo()
+        {
+            return "nVidia GeForce 650";
+        }
+
+        public void DrawRectangle(int width, int height)
+        {
+            // doing stuff
+
+            Console.WriteLine("Drawing specified rectangle on canvas using legacy rectangle");
+        }
+
+        public void DrawCircle(int width, int height)
+        {
+            Console.WriteLine("Drawing specified circle on canvas using legacy rectangle");
+        }
+
+        public void DrawTriangle(int width, int height)
+        {
+            Console.WriteLine("Drawing specified traingle on canvas using legacy rectangle");
+        }
+    }
+}
---
+++ 06Adapter/Students/2017/ZukAdam/AdapterExample/AdapterExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AdapterExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adapter = new GfxAdapter(new NewGfxLibrary());
+            Console.WriteLine(adapter.Draw(new Rectangle()));
+
+            adapter = new GfxAdapter(new LegacyGfxLibrary());
+            Console.WriteLine(adapter.Draw(new Rectangle()));
+
+            Console.ReadKey();
+        }
+    }
+}
