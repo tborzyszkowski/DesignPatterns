d9f40217fa72e7f7f8c1e2f4072af1e55e678766
aneta-7 <aneta.stypa7@wp.pl>
2017-12-16 08:33:23 +0100

    Adapter design pattern

---
+++ 06Adapter/Students/2017/AStypa/Adapter/Adapter/PersonJSONAdaptee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Adapter
+{
+    public class PersonJSONAdaptee
+    {
+        public string GetPersonListJSON()
+        {
+            List<Person> personList = new List<Person>();
+            personList.Add(new Person
+            {
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                Age = 43
+            });
+
+            personList.Add(new Person
+            {
+                FirstName = "Alicja",
+                LastName = "Nowak",
+                Age = 23
+            });
+
+            personList.Add(new Person {
+                FirstName = "Andrzej",
+                LastName = "Drzewo",
+                Age = 34
+            });
+
+            dynamic collectionPerson = new
+            {
+                person = personList
+            };
+
+            return JsonConvert.SerializeObject(collectionPerson);
+        }
+    }
+}
