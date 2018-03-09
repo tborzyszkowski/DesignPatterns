af1f28489b8a3be28a52e75463eab6bdd73f65b8
Artur Koliński <secorek@gmail.com>
2018-03-09 15:19:10 +0100

    added singleton with tests

---
+++ 01Singleton/Students/2018/artkolinski/01Singleton/Singleton/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton
+{
+    [Serializable]
+    public class Product
+    {
+        public string Name;
+        public int Price;
+        public Product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
