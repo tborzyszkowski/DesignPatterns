cfdf0c10c4633e9495eb8e4d73d7d33646e7435c
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-04-06 11:42:10 +0200

    Dekorator dodatkowe projekty

---
+++ 07Dekorator/04IODekoratorUsage/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace _04IODekoratorUsage {
+    //
+    // See: https://www.productivecsharp.com/2012/09/decorator-pattern-streams-in-net/
+    //
+    class Program {
+        static void Main() {
+            RunServer();
+            SendMessage("Hello, I am sending this message using the decorator pattern.");
+        }
+
+        private static void RunServer() {
+            var tcpListener = new TcpListener(IPAddress.Any, 8888);
+
+            new Thread(() =>
+            {
+                tcpListener.Start();
+                var client = tcpListener.AcceptTcpClient();
+
+                // Configure the receiving pipiline of streams
+                var networkStream = client.GetStream();
+                var cryptoStream = new CryptoStream(networkStream, new SHA512Managed(), CryptoStreamMode.Read);
+                var gzipStream = new GZipStream(cryptoStream, CompressionMode.Decompress);
+                var bufferedStream = new BufferedStream(gzipStream, 64);
+
+                var fileStream = new FileStream("result.txt", FileMode.Create);
+
+                using (bufferedStream)
+                {
+                    using (fileStream)
+                    {
+                        while (true)
+                        {
+                            int data = bufferedStream.ReadByte();
+                            if (data == -1) break;
+
+                            fileStream.WriteByte((byte)data);
+                        }
+                    }
+                }
+
+                Process.Start("result.txt");
+            }).Start();
+
+            Thread.Sleep(1000);
+        }
+
+        private static void SendMessage(string message) {
+            var client = new TcpClient("localhost", 8888);
+
+            // Configure the sending pipiline of streams
+            var networkStream = client.GetStream();
+            var cryptoStream = new CryptoStream(networkStream, new SHA512Managed(), CryptoStreamMode.Write);
+            var gzipStream = new GZipStream(cryptoStream, CompressionMode.Compress);
+            var bufferedStream = new BufferedStream(gzipStream, 64);
+
+            using (bufferedStream)
+            {
+                foreach (var b in Encoding.Unicode.GetBytes(message))
+                {
+                    bufferedStream.WriteByte(b);
+                }
+            }
+        }
+    }
+}
---
+++ 07Dekorator/05TextValidationUWP/Components/TextBoxComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Windows.UI.Xaml.Controls;
+
+namespace _05TextValidationUWP.Components {
+    public class TextBoxComponent : ITextBoxComponent {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected readonly TextBox _textBox;
+
+        public TextBoxComponent(TextBox textBox) {
+            _textBox = textBox;
+        }
+
+        public string Text {
+            get { return _textBox.Text; }
+            set {
+                _textBox.Text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        protected void OnPropertyChanged(string name) {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
