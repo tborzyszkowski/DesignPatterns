b97edb41cb75b48745d267365cd2c4d04785e013
filip <filip.dabrowski.pg@gmail.com>
2018-11-12 21:31:38 +0100

    Factory implementation

---
+++ 02Fabryka/Students/2018/DabrowskiFilip/lab2factory/src/main/java/cars/mercedes/MercedesLimo.java
@@ -0,0 +1,13 @@
+package cars.mercedes;
+
+import cars.Car;
+
+public class MercedesLimo extends Car {
+
+    public MercedesLimo() {
+        this.name = "mercedes";
+        this.model = "S class";
+        this.productionYear = 2014;
+        this.gearBoxType = "automatic";
+    }
+}
---
+++ 02Fabryka/Students/2018/DabrowskiFilip/lab2factory/src/main/java/fabricMethodsFactories/FordFactory.java
@@ -0,0 +1,31 @@
+package fabricMethodsFactories;
+
+import cars.Car;
+import cars.ford.*;
+import utils.CarType;
+
+public class FordFactory extends CarFactory {
+
+    private static FordFactory carFactory;
+
+    public static FordFactory getInstance() {
+        if (carFactory == null) {
+            carFactory = new FordFactory();
+        }
+        return carFactory;
+    }
+
+    Car createCar(CarType carType) {
+        if (carType.equals(CarType.SPORTS_CAR)) {
+            return new FordSportsCar();
+        } else if (carType.equals(CarType.SEDAN)) {
+            return new FordSedan();
+        } else if (carType.equals(CarType.SUV)) {
+            return new FordSuv();
+        } else if (carType.equals(CarType.LIMO)) {
+            return new FordLimo();
+        } else if (carType.equals(CarType.HATCH_BAG)) {
+            return new FordHatchBag();
+        } else return null;
+    }
+}
