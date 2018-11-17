23a69012af4deac4ebd37fd897eb6853ed50ba29
Marcin <marcinmug@gmail.com>
2018-11-17 17:26:48 +0100

    Factory Marcin Mokrzan

---
+++ 02Fabryka/Students/2018/MarcinMokrzan/ClassRegistrationFactory/src/classregistrationfactory/GraOTor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classregistrationfactory;
+
+/**
+ *
+ * @author Marcin
+ */
+public class GraOTor extends Game {
+    
+    public GraOTor () {
+        this.productName="GraOTor"; 
+        this.type="Gra karciana";
+        this.time="ok. 30minut";
+        this.players="2-4 osób";
+        this.age="od 8 lat";
+    }
+}
---
+++ 02Fabryka/Students/2018/MarcinMokrzan/ClassRegistrationFactory/src/classregistrationfactory/HejToMojaRyba.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classregistrationfactory;
+
+/**
+ *
+ * @author Marcin
+ */
+public class HejToMojaRyba extends Game {
+    
+    public HejToMojaRyba () {
+        this.productName="HejToMojaRyba"; 
+        this.type="Gra planszowa: familijna";
+        this.time="ok. 20 minut";
+        this.players=" 2-4 osoby";
+        this.age="od 8 lat";
+    }
+    
+}
