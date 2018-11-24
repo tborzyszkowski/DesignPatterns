1155a39184f15ec3a877eb1d3425b10f0da001c3
Marek <marekegg94@gmail.com>
2018-11-24 10:05:07 +0100

    singleton i fabryka

---
+++ 02Fabryka/Students/2018/meggert/Factory/AbstractFactory/Model/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace AbstractFactory.Model
+{
+    public class Car : BaseVehicle
+    {
+        public override string Name => "Car";
+        public override int Wheels => 4;
+        public override bool HasEngine => true;
+    }
+}
