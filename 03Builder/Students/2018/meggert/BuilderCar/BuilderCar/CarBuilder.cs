ca09470bd338a6f1d5a67435180053817828762a
Marek <marekegg94@gmail.com>
2018-12-08 11:15:38 +0100

    builder i prototype

---
+++ 03Builder/Students/2018/meggert/BuilderCar/BuilderCar/CarBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBuilder
+{
+    public class CarBuilder
+    {
+        string Name { get; set; }
+        string Engine { get; set; }
+        Type TypeCar { get; set; }
+        string Battery { get; set; }
+
+        public CarBuilder CreateCar(string name)
+        {
+            this.Name = name;
+            return this;
+        }
+
+        public CarBuilder InsertEngine(string engine)
+        {
+            this.Engine = engine;
+            return this;
+        }
+
+        public CarBuilder InsertTypeCar(Type typeCar)
+        {
+            this.TypeCar = typeCar;
+            return this;
+        }
+
+        public CarBuilder InsertBattery (string battery)
+        {
+            this.Battery = battery;
+            return this;
+        }
+
+        public Car Build()
+        {
+            return new Car(Name, Engine, TypeCar, Battery);
+        }
+
+        public static implicit operator Car(CarBuilder tb)
+        {
+            return new Car(
+                tb.Name,
+                tb.Engine,
+                tb.TypeCar,
+                tb.Battery);
+        }
+
+    }
+}
