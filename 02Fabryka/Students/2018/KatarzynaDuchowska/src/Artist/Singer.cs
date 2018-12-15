2d51e2aec5aae31cd354863e49fc031ef178959f
Katarzyna Duchowska <k.duchowska@o2.pl>
2018-12-15 18:34:50 +0100

    factory

---
+++ 02Fabryka/Students/2018/KatarzynaDuchowska/src/Artist/Singer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.Artist
+{
+    public class Singer : IArtist
+    {
+        public void DoArt()
+        {
+            Console.WriteLine("I am a singer");
+        }
+    }
+}
---
+++ 02Fabryka/Students/2018/KatarzynaDuchowska/test/FactoryMethodTest.cs
@@ -0,0 +1,55 @@
+﻿using Factory;
+using Factory.Doctor;
+using Factory.Manufacturing;
+using Factory.Teacher;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass]
+    public class FactoryMethodTest
+    {
+        [TestMethod]
+        public void DentistCreationTest()
+        {
+            FactoryMethod factory = DoctorFactory.GetFactory();
+            IPerson x = factory.GetPerson<Pneumologist>();
+
+            Assert.AreSame(x.GetType(), typeof(Pneumologist));
+        }
+
+        [TestMethod]
+        public void DentistCreationWithoutReflectionTest()
+        {
+            FactoryMethod factory = DoctorFactory.GetFactory();
+            IPerson x = factory.GetPersonWithoutReflection(typeof(Pneumologist));
+
+            Assert.AreSame(x.GetType(), typeof(Pneumologist));
+        }
+
+
+        [TestMethod]
+        public void GermanTeacherCreationTest()
+        {
+            FactoryMethod factory = TeacherFactory.GetFactory();
+            IPerson x = factory.GetPerson<GermanTeacher>();
+
+            Assert.AreSame(x.GetType(), typeof(GermanTeacher));
+        }
+
+        [TestMethod]
+        public void GermanTeacherCreationWithoutReflectionTest()
+        {
+            FactoryMethod factory = TeacherFactory.GetFactory();
+            IPerson x = factory.GetPersonWithoutReflection(typeof(GermanTeacher));
+
+            Assert.AreSame(x.GetType(), typeof(GermanTeacher));
+        }
+    }
+}
+
