b942b42cf516405c4e272692a94b9d856fc2a5be
Katarzyna Duchowska <k.duchowska@o2.pl>
2018-12-30 17:18:42 +0100

    prototype

---
+++ 04Prototype/Students/2018/DuchowskaKatarzyna/zad2/Price.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.zad2
+{
+    [Serializable()]
+    public class Price
+    {
+        public double Amount { get; set; }
+        public Currency Currency { get; set; }
+
+        public Price(double amount)
+        {
+            Amount = amount;
+            Currency = new Currency();
+        }
+    }
+}
---
+++ 04Prototype/Students/2018/DuchowskaKatarzyna/zad2/Prototype.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.zad2
+{
+    [Serializable()]
+    public class Prototype<T>
+    {
+        // shallow copy
+        public T ShallowCopy()
+        {
+            return (T) MemberwiseClone();
+        }
+
+        // deep Copy
+        public T DeepCopy()
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, this);
+            stream.Seek(0, SeekOrigin.Begin);
+            T copy = (T)formatter.Deserialize(stream);
+            stream.Close();
+            return copy;
+        }
+    }
+}
+
