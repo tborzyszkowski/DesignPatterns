7f3492281ef3a6714ceea86e18e5440eea107d05
Marek <marekegg94@gmail.com>
2019-01-11 16:31:43 +0100

    projekt

---
+++ ProjektyStudentow/2019_Marek_Eggert/FactoryCar/FactoryCar/Presenter/CarPresenter.cs
@@ -0,0 +1,64 @@
+﻿using FactoryCar.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryCar.Presenter
+{
+    public class CarPresenter
+    {
+        public CarDocument document = new CarDocument();
+        
+        public void OnInit()
+        {
+            document.CarList = new List<Car>();
+            document.CarList = CarSingleton.Instance.GetCar();
+        }
+
+        public List<Car> AddNewProduct(Car product)
+        {
+            CarSingleton.Instance.AddProduct(product);
+           
+            return CarSingleton.Instance.GetCar();
+        }
+
+        public List<Car> Calulate(string c)
+        {
+            var tmp = new List<Car>();
+            tmp = CarSingleton.Instance.GetCar();
+
+            ContextStrategy context = null;
+            switch (c) {
+                case "Kmh":
+                  context = new ContextStrategy(new Kmh());
+                  break;
+                case "Mph":
+                    context = new ContextStrategy(new Mph());
+                    break;
+                default:
+                   break;
+            }
+            return context.Calculate(tmp);
+        }
+
+        public void WriteToCSV()
+        {
+            CSVAdaptee csv = new CSVAdaptee();
+            csv.ToCSV(document.CarList);
+        }
+
+        public void WriteToTxt()
+        {
+            TxtAdaptee txt = new TxtAdaptee();
+            txt.ToTxt(document.CarList);
+        }
+
+    }
+
+    public class CarDocument
+    {
+        public List<Car> CarList { get; set; }
+    }
+}
---
+++ ProjektyStudentow/2019_Marek_Eggert/FactoryCar/FactoryCar/Program.cs
@@ -0,0 +1,20 @@
+﻿using FactoryCar.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactoryCar
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ProductListView());
+        }
+    }
+}
