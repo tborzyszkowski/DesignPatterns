ae0c3265c2026764bbf27eb7da934491c102d552
RGolun <rgouski@gmail.com>
2019-03-03 10:56:11 +0100

    rgolunski

---
+++ Zadania/EGZAMIN/EGZAMIN/Megatron.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGZAMIN
+{
+    class Megatron
+    {
+        protected List<string> Raport = new List<string>();
+        protected string wplyw;
+
+
+        public void Info()
+        {
+            foreach (var dd in wplyw)
+            {
+                Console.WriteLine(dd);
+            }
+        }
+    }
+}
---
+++ Zadania/EGZAMIN/EGZAMIN/TxtRaport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EGZAMIN
+{ 
+
+public class TxtRaport : Raport
+{
+    public void generateRaport(Cart cart)
+    {
+            string path = @"c:\temp\MyTest.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                  
+                }
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+
+    }
+
+}
+
