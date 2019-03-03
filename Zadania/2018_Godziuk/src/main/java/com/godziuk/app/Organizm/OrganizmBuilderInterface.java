39e64b14cf70895fefd51e2211def759138e41fc
tborzyszkowski <t.borzyszkowski@gmail.com>
2019-03-03 09:42:12 +0100

    Godiuk remove

---
+++ Zadania/2018_Godziuk/src/main/java/com/godziuk/app/Organizm/OrganizmBuilderInterface.java
@@ -0,0 +1,17 @@
+package com.godziuk.app.Organizm;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jgodziuk
+ */
+public interface OrganizmBuilderInterface {
+    public void buildOrganizmType();
+    
+    public Organizm getOrganizm();
+}
