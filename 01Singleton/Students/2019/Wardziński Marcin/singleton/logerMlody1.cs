85b6fc029dae09f8e2ffb530b201451ccaf75ab8
wardasz <m.wardzinski5991@gmail.com>
2019-03-15 15:09:40 +0100

    singleton Marcin Wardziński

---
+++ 01Singleton/Students/2019/Wardziński Marcin/singleton/logerMlody1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class logerMlody1 : logerStary1
+    {
+        //private logerMlody1() : base() { }
+
+        new public void loguj()
+        {
+            Console.WriteLine("syn jeden pisze");
+        }
+    }
+}
---
+++ 01Singleton/Students/2019/Wardziński Marcin/singleton/logerStary1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class logerStary1 : logerBaza<logerStary1>
+    {
+        //private logerStary1() : base() { }
+
+        new public void loguj()
+        {
+            Console.WriteLine("ociec jeden pisze");
+        }
+    }
+}
