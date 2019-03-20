04096208666176419ca5d575a71fe768635cef08
mmazepa <maksymilianmariusz@gmail.com>
2019-03-20 21:52:51 +0100

    Singleton - rozwiązanie (Maven, JUnit).

--- 01Singleton/Students/2019/MazepaMariusz/SingletonTest.java
+++
@@ -1,7 +0,0 @@
-class SingletonTest {
-  public static void main(String[] args) {
-    Singleton instance1 = Singleton.getInstance();
-    Singleton instance2 = Singleton.getInstance();
-    Helper.compareInstances(instance1, instance2);
-  }
-}
--- 01Singleton/Students/2019/MazepaMariusz/ThreadTest.java
+++
@@ -1,9 +0,0 @@
-class ThreadTest implements Runnable {
-  @Override
-  public void run() {
-    String threadName = Thread.currentThread().getName();
-    int hashCode = Singleton.getInstance().hashCode();
-    SingletonTest2.hm.put(threadName, hashCode);
-    System.out.println(threadName + " : " + hashCode);
-  }
-}
