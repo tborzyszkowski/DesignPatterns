f0a5edb980a3031a8e1a79f459a669f2e0b7c9a3
Mikolaj Piecuch <mikolajpiecuch1@gmail.com>
2019-04-04 10:40:39 +0200

    Singleton project

---
+++ 01Singleton/Students/2019/Piecuch Mikołaj/Singleton/Singleton/SingletonBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class SingletonBase
+    {
+        private static SingletonBase instance;
+        private static object instance_lock = new object();
+
+        protected SingletonBase()
+        {
+
+        }
+
+        public static SingletonBase GetInstance<T>() where T : SingletonBase
+        {
+            if (instance != null)
+                return instance;
+
+            lock (instance_lock)
+            {
+                if (instance != null)
+                    return instance;
+
+                instance = Activator.CreateInstance(typeof(T), true) as SingletonBase;
+                return instance;
+            }
+        }
+
+        public virtual string TestMethod()
+        {
+            return $"Test method from abstract Singleton<T> class.";
+        }
+    }
+}
