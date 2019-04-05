657dc9f90469cbee8fbabebf0ce972ab8b0e21cb
Błażej <kucmeno12@o2.pl>
2019-04-05 15:34:21 +0200

    add PrototypePattern

---
+++ 04Prototype/Students/2019/KucmanBłażej/PrototypePattern/PrototypePattern/exercise2/Chipset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern.exercise2
+{
+    [Serializable()]
+    public class Chipset
+    {
+        public Chipset(string namen, Producent producent)
+        {
+            name = name;
+            this.producent = producent;
+        }
+
+        public string name { get; set; }
+        public Producent producent { get; set; }
+    }
+}
---
+++ 04Prototype/Students/2019/KucmanBłażej/PrototypePattern/PrototypePattern/exercise2/Motherboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern.exercise2
+{
+    [Serializable()]
+    public class Motherboard
+    {
+        public Motherboard(string name, Chipset chipset)
+        {
+            this.name = name;
+            this.chipset = chipset;
+        }
+
+        public string name { get; set; }
+        public Chipset chipset { get; set; }
+    }
+}
