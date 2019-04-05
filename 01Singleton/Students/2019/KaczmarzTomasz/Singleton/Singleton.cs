4bdd5fa0e5373ce2996b87aebdd944538af33a88
tkaczmarz <kaczmarz.tomasz@gmail.com>
2019-04-05 16:18:14 +0200

    Tomasz Kaczmarz Singleton

---
+++ 01Singleton/Students/2019/KaczmarzTomasz/Singleton/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+	public sealed class Singleton
+	{
+		public static int counter = 0;
+
+		private static Singleton instance = null;
+		private static readonly object padlock = new object();
+
+		public static Singleton Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					lock (padlock)
+					{
+						if (instance == null)
+						{
+							instance = new Singleton();
+							Console.WriteLine("Singleton instance initiated");
+						}
+					}
+				}
+				
+				return instance;
+			}
+		}
+
+		private Singleton() { counter++; }
+	}
+}
