5da3dec1192a582c10f25a15441beeff8e41e27f
M. Barzowska <barzowskam@gmail.com>
2019-04-18 00:03:27 +0200

    add mbarzowska singleton

---
+++ 01Singleton/Students/2019/BarzowskaMonika/Inheritance/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public abstract class Singleton<T> where T : class
+    {
+        protected static readonly Lazy<T> 
+            _instance = 
+                new Lazy<T>(
+                () => Activator.CreateInstance(typeof(T), nonPublic: true) as T, LazyThreadSafetyMode.ExecutionAndPublication
+                );
+
+        public static T Instance => _instance.Value;
+
+        protected Singleton()
+        {
+            Counter++;
+        }
+
+        public static int Counter;
+    }
+}
---
+++ 01Singleton/Students/2019/BarzowskaMonika/ThreadSafety/SimpleLockSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSafety
+{
+    public class SimpleLockSingleton
+    {
+        private static SimpleLockSingleton _instance;
+        private static readonly object _padlock = new object();
+
+        public static SimpleLockSingleton Instance
+        {
+            get
+            {
+                lock (_padlock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SimpleLockSingleton();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        private SimpleLockSingleton()
+        {
+        }
+    }
+}
