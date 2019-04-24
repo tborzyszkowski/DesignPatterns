c9f2a2e914c13f9ef5bae12697858d459c51b022
AliaksandrN <novik.alexander.88@gmail.com>
2019-04-24 20:33:40 +0200

    project init

---
+++ 03Builder/Students/2019/NovikAliaksandr/src/main/java/ug/anovik/NovikAliaksandr/App.java
@@ -0,0 +1,13 @@
+package ug.anovik.NovikAliaksandr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
