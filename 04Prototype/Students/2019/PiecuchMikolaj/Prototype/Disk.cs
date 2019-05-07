5bdf0b72a36589859cbde98c44b9fa0ef0abf3bc
Mikolaj Piecuch <mikolajpiecuch1@gmail.com>
2019-05-07 21:43:50 +0200

    Prototype (zad. 2)

---
+++ 04Prototype/Students/2019/PiecuchMikolaj/Prototype/Disk.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Disk : AbstractCloneable<Disk>
+    {
+        public long Size { get; set; }
+        public bool Rotational { get; set; }
+
+        public override void HandleDeepClone(Disk cloned)
+        {
+            //nothing to deep copy
+        }
+
+
+    }
+}
