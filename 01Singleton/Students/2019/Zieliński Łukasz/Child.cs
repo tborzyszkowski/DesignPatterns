39d5975064c94ecf8c4195493d8130c55863830b
Łukasz <lzielinski@sigma.ug.edu.pl>
2019-05-09 13:22:17 +0200

    Singleton

---
+++ 01Singleton/Students/2019/Zieliński Łukasz/Child.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Singleton;
+
+namespace Singleton
+{
+    public class Child : SingletonInheritance<Child>
+    {
+        protected Child() : base()
+        {
+
+        }
+    }
+}
