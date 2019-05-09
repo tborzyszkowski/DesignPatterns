0e3a9c8420f55ce929972bca0f3f75a7cbcf12ed
Rilok <32479464+Rilok@users.noreply.github.com>
2019-05-09 11:13:18 +0200

    Commit Factory Pattern

---
+++ 02Fabryka/Students/2019/CimochowskiSzymon/Factory/SimpleFactory/PCs/HPPC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factory.SimpleFactory.PCs
+{
+    class HPPC : PC
+    {
+        public HPPC()
+        {
+            Manufacturer = "HP";
+            Model = "Desktop Pro G2";
+            Country = "USA";
+            ProdYear = 2019;
+            Price = 2399;
+            CaseType = "ATX Desktop";
+        }
+
+        public new void setUp()
+        {
+            Console.WriteLine("Setting up HP PC software.");
+        }
+
+        public new void build()
+        {
+            Console.WriteLine("Building HP PC construction.");
+        }
+    }
+}
---
+++ 02Fabryka/Students/2019/CimochowskiSzymon/Factory/SimpleFactory/PCs/KomputronikPC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Factory.SimpleFactory.PCs
+{
+    class KomputronikPC : PC
+    {
+        public KomputronikPC()
+        {
+            Manufacturer = "Komputronik";
+            Model = "XT50";
+            Country = "Poland";
+            ProdYear = 2018;
+            Price = 4499;
+            CaseType = "ATX Full Tower";
+        }
+
+        public new void setUp()
+        {
+            Console.WriteLine("Setting up Komputronik PC software.");
+        }
+        public new void build()
+        {
+            Console.WriteLine("Building Komputronik PC construction.");
+        }
+    }
+}
