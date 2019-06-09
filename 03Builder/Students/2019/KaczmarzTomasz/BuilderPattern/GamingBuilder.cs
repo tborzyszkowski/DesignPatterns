d28aa1c41441e5f70a620a9cf2ee70119cead4d9
tkaczmarz <kaczmarz.tomasz@gmail.com>
2019-06-09 18:37:49 +0200

    Added builder pattern

---
+++ 03Builder/Students/2019/KaczmarzTomasz/BuilderPattern/GamingBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    class GamingBuilder : ComputerBuilder
+    {
+        public GamingBuilder()
+        {
+            computer = new Computer(ComputerType.GamingPC);
+        }
+
+        public override ComputerBuilder BuildCPU()
+        {
+            computer[Part.CPU] = "Intel Core i9-9900K";
+            return this;
+        }
+
+        public override ComputerBuilder BuildGPU()
+        {
+            computer[Part.GPU] = "Nvidia GeForce RTX 2080Ti";
+            return this;
+        }
+
+        public override ComputerBuilder BuildMotherboard()
+        {
+            computer[Part.Motherboard] = "Gigabyte Z390 AORUS XTREME";
+            return this;
+        }
+
+        public override ComputerBuilder BuildRAM()
+        {
+            computer[Part.RAM] = "G.SKILL Trident Z 64GB 3200MHz";
+            return this;
+        }
+    }
+}
---
+++ 03Builder/Students/2019/KaczmarzTomasz/BuilderPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+
+            shop.Construct(new OfficeBuilder()).Show();
+            shop.Construct(new MidRangeBuilder()).Show();
+            shop.Construct(new GamingBuilder()).Show();
+
+            Console.ReadKey();
+        }
+    }
+}
