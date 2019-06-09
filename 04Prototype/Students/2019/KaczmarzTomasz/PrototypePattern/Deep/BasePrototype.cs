ec70ebff563baebf6ac841d360b022fe9b5a76f6
tkaczmarz <kaczmarz.tomasz@gmail.com>
2019-06-09 22:56:26 +0200

    Added deep copy for prototype pattern

---
+++ 04Prototype/Students/2019/KaczmarzTomasz/PrototypePattern/Deep/BasePrototype.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern.Deep
+{
+    [Serializable]
+    abstract class BasePrototype<T>
+    {
+        public T Clone()
+        {
+            return (T)this.MemberwiseClone();
+        }
+
+        public T DeepCopy()
+        {
+            T copy;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, this);
+                stream.Seek(0, SeekOrigin.Begin);
+                copy = (T)formatter.Deserialize(stream);
+            }
+            return copy;
+        }
+    }
+}
---
+++ 04Prototype/Students/2019/KaczmarzTomasz/PrototypePattern/Deep/Fan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern.Deep
+{
+    [Serializable]
+    class Fan
+    {
+        public int diameter;
+
+        public Fan(int diameter)
+        {
+            this.diameter = diameter;
+        }
+    }
+}
