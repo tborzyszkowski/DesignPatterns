6b055594a163e07d1ebc4d8b832c6f6564a4c3d8
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2025-06-13 19:25:09 +0200

    CQRS sample

---
+++ A01CQRSPattern/Application/QueriesMediatR/GetProductsByName/GetProductsByNameQueryHandler.cs
@@ -0,0 +1,48 @@
+﻿using Application.Common;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.QueriesMediatR
+{
+    public class GetProductsByNameQueryHandler : IRequestHandler<GetProductsByNameQuery, List<ProductDisplay>>
+    {
+        private readonly IApplicationContextInMemoryDB _context;
+
+        public GetProductsByNameQueryHandler(IApplicationContextInMemoryDB context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ProductDisplay>> Handle(GetProductsByNameQuery request, CancellationToken cancellationToken)
+        {
+
+            var products =  await _context.Products.Where(p => p.Name.Contains(request.Name, StringComparison.OrdinalIgnoreCase)).ToListAsync();
+
+            if (products == null)
+                return null;
+
+            var results = new List<ProductDisplay>();
+            foreach (var p in products)
+            {
+                var productDisplay = new ProductDisplay
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Description = p.Description,
+                    UnitPrice = p.UnitPrice,
+                    IsOutOfStock = p.IsOutOfStock
+                };
+                results.Add(productDisplay);
+
+            }
+            return results;
+        }
+    }
+}
---
+++ A01CQRSPattern/THANKS.md
@@ -0,0 +1,2 @@
+Ten przykład został zainspirowany repozytorium:
+[https://github.com/AbdelmajidBa/CQRSPattern](https://github.com/AbdelmajidBa/CQRSPattern).
